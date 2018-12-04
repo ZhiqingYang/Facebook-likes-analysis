a299a6c267b7ca581ae87690b0eaa4f2d1aea80c
yang05-1760239 <yang05@uw.edu>
2018-12-03 16:31:47 -0800

    add color

--- gender_vs_likes.R
+++ gender_vs_likes.R
@@ -1,15 +1,15 @@
 library(dplyr)
 library(ggplot2)
 
-facebook_info <- read.csv(file = "../facebook.csv", 
+facebook_info <- read.csv(file = "./facebook.csv", 
                      stringsAsFactors = FALSE)
 
 gender_likes_graph <- function(this_data)
     ggplot() + 
       geom_bar(data = this_data, aes(x = gender, y = mean_likes
-      ), stat = 'identity') +
-      ggtitle(paste('gender vs likes')) +
-      xlab("gender") +
-      ylab("mean") 
+      ), stat = 'identity') + scale_fill_manual(values = c("red", "blue")) +
+      ggtitle(paste('gender vs likes received')) +
+      xlab("gender") 
+      ylab("mean of likes received ") 
 
 

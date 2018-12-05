4371e4e50615738ad85bc973c3787b7aaddad695
MichelleChen <mc0504@uw.edu>
2018-12-05 00:37:56 -0800

    final waiting for deletion

--- facebook/server.R
+++ facebook/server.R
@@ -3,35 +3,46 @@ library(ggplot2)
 library(dplyr)
 library(plotly)
 library(lettercase)
+library(RColorBrewer)
 
-facebook <- read.csv("../facebook.csv", stringsAsFactors = FALSE)
-labs <- c(paste(seq(10, 65, by = 10), seq(10 + 10 - 1, 70 - 1, by = 10), sep = "-"), paste(70, "+", sep = ""))
-facebook$AgeGroup <- cut(facebook$age, breaks = c(seq(10, 70, by = 10), Inf), labels = labs, right = FALSE)
+facebook <- read.csv("facebook.csv", stringsAsFactors = FALSE)
+labs <- c(paste(seq(10, 65, by = 10), seq(10 + 10 - 1, 70 - 1, by = 10),
+  sep = "-"), paste(70, "+", sep = ""))
+facebook$AgeGroup <- cut(facebook$age, breaks = c(
+  seq(10, 70, by = 10),
+  Inf), labels = labs, right = FALSE)
 
 server <- function(input, output) {
-  
   output$barplot <- renderPlotly({
     if (input$device == "Mobile") {
-      likes_mean <- select(facebook, gender, mobile_likes_received) %>% 
-        group_by(gender) %>% 
-        summarise(mean_likes = mean(mobile_likes_received)) %>% 
+      likes_mean <- select(facebook, gender, mobile_likes_received) %>%
+        group_by(gender) %>%
+        summarise(mean_likes = mean(mobile_likes_received)) %>%
         filter(gender == "female" | gender == "male")
     } else if (input$device == "Web") {
-      likes_mean <- select(facebook, gender, www_likes_received) %>% 
-        group_by(gender) %>% 
-        summarise(mean_likes = mean(www_likes_received)) %>% 
+      likes_mean <- select(facebook, gender, www_likes_received) %>%
+        group_by(gender) %>%
+        summarise(mean_likes = mean(www_likes_received)) %>%
         filter(gender == "female" | gender == "male")
     } else {
-      likes_mean <- select(facebook, gender, likes_received) %>% 
-        group_by(gender) %>% 
-        summarise(mean_likes = mean(likes_received)) %>% 
+      likes_mean <- select(facebook, gender, likes_received) %>%
+        group_by(gender) %>%
+        summarise(mean_likes = mean(likes_received)) %>%
         filter(gender == "female" | gender == "male")
     }
-    plot_ly(likes_mean) %>% 
-      add_trace(x = ~gender, y = ~mean_likes, width = 0.6, text = ~round(mean_likes), textposition = "auto", textfont = list(color = "white"), type = "bar", width = ~1.0, marker = list(color = c("red", "blue"))) %>% 
-      layout(title = paste0("Gender vs. Likes on ", input$device), xaxis = list(title = "Gender"), yaxis = list(title = "Mean Likes"))
+    plot_ly(likes_mean) %>%
+      add_trace(
+        x = ~gender, y = ~mean_likes, width = 0.6,
+        text = ~round(mean_likes), textposition = "auto",
+        textfont = list(color = "white"), type = "bar",
+        width = ~1.0, marker = list(color = c("red", "blue"))
+      ) %>%
+      layout(
+        title = paste0("Gender vs. Likes on ", input$device),
+        xaxis = list(title = "Gender"), yaxis = list(title = "Mean Likes")
+      )
   })
-  
+
   output$scatterplot1 <- renderPlotly({
     this_age <- filter(facebook, AgeGroup == input$age_friendships)
     if (input$friendships == "Friendships initiated") {
@@ -41,17 +52,25 @@ server <- function(input, output) {
       age_chart <- group_by(this_age, age) %>%
         summarize(median = median(friend_count))
     }
-    #global_colors <- setNames(rainbow(nrow(age_chart)), input$age_friendships)
-    pal <- c("red", "blue", "yellow", "green", "black", "purple", "pink")
-    plot_ly(data = age_chart, x = ~age, y = ~median, type = 'scatter', mode = 'markers', color = ~input$age_friendships, colors = pal) %>% 
-      layout(title = str_title_case(paste0("Relationship Between Age and ", input$friendships)), 
-                   yaxis = list(title = str_title_case(paste0("median ", input$friendships))), xaxis = list(title = "Age"))
+    plot_ly(data = age_chart, x = ~age, y = ~median, type = "scatter",
+      mode = "markers", color = ~age, colors = brewer.pal(7, "Paired")) %>%
+      layout(
+        title = str_title_case(paste0("Relationship Between Age and ",
+                                      input$friendships)),
+        yaxis = list(title = str_title_case(paste0("median ",
+                        input$friendships))), xaxis = list(title = "Age")
+      )
   })
-  
+
   output$scatterplot2 <- renderPlotly({
-    without_outliers <- filter(facebook, likes < 10000, likes_received > 0 & likes_received < 10000)
+    without_outliers <- filter(facebook, likes < 10000, likes_received > 0
+                               & likes_received < 10000)
     this_age <- filter(without_outliers, AgeGroup == input$age_likes)
-    plot_ly(data = this_age, x = ~likes, y = ~likes_received, type = 'scatter', mode = 'markers') %>% 
-      layout(title = "Likes vs. Likes Received", xaxis = list(title = "Mean Likes"), yaxis = list(title = "Mean Likes Received"))
+    plot_ly(data = this_age, x = ~likes, y = ~likes_received, type = "scatter",
+            mode = "markers", mode = "markers", color = ~AgeGroup,
+            colors = brewer.pal(7, "Paired")) %>%
+      layout(title = "Likes vs. Likes Received",
+             xaxis = list(title = "Mean Likes"),
+             yaxis = list(title = "Mean Likes Received"))
   })
 }
--- facebook/ui.R
+++ facebook/ui.R
@@ -2,32 +2,43 @@ library(shiny)
 library(shinythemes)
 library(plotly)
 
-facebook <- read.csv("../facebook.csv", stringsAsFactors = FALSE)
-
 navbarPage("Facebook",
   theme = shinytheme("flatly"),
   tabPanel("Overview",
     sidebarLayout(
       sidebarPanel(
-        p("Facebook data is available ", a("here.", href = "https://www.kaggle.com/sheenabatra/facebook-data")),
+        p("Facebook data is available ", a("here.",
+          href = "https://www.kaggle.com/sheenabatra/facebook-data")),
         p("Major Libraries Used: ", tags$b("plotly, dplyr, shinythemes")),
         p("Data Reading: ", tags$b("csv"))
       ),
       mainPanel(
-        img(src = "../facebook.jpg"),
         h3("Project Overview"),
-        p("For this project we used a dataset from Sheena Batra, a software engineer who published the data on Kaggle, an online community for data scientists and machine learners owned by Google. This dataset provides insight on Facebook users statistics and demographics."),
+        p(paste0("For this project we used a dataset from Sheena Batra, a ",
+                 "software engineer who published the data on Kaggle, an ",
+                 "online community for data scientists and machine learners ",
+                 "owned by Google. This dataset provides insight on Facebook ",
+                 "users statistics and demographics.")),
         br(),
         h3("Target Audience"),
-        p("Our target audience are people who use social media and technology a lot, specifically Facebook, and are interested in learning more about the correlations of the different factors of this data such as gender and age group."),
+        p(paste0("Our target audience are people who use social media and ",
+                 "technology a lot, specifically Facebook, and are interested ",
+                 "in learning more about the correlations of the different ",
+                 "factors of this data such as gender and age group.")),
         br(),
         h3("Central Questions"),
-        p("Some of the central questions that we are trying to answer through our interpretation include:"),
+        p(paste0("Some of the central questions that we are trying to answer ",
+                 "through our interpretation include:")),
         tags$ul(
-          tags$li("What gender on average receives the most likes on their profile?"),
-          tags$li("What is the correlation between age group and friendships initiated/friend count?"),
-          tags$li("What are the demographics of Facebook users who are likely to interact on the platform through likes and friendships?"),
-          tags$li("Can we determine how strong the number of likes a user gives correlates with the likes they receive?")
+          tags$li(paste0("What gender on average receives the most likes on ",
+                         "their profile?")),
+          tags$li(paste0("What is the correlation between age group and ",
+                         "friendships initiated/friend count?")),
+          tags$li(paste0("What are the demographics of Facebook users who are ",
+                         "likely to interact on the platform through likes ",
+                         "and friendships?")),
+          tags$li(paste0("Can we determine how strong the number of likes a ",
+                         "user gives correlates with the likes they receive?"))
         ),
         br(),
         h3("Project Creators"),
@@ -40,7 +51,7 @@ navbarPage("Facebook",
       )
     )
   ),
-  
+
   tabPanel("Gender and Likes",
     sidebarLayout(
       sidebarPanel(
@@ -48,41 +59,79 @@ navbarPage("Facebook",
                      c("Mobile", "Web", "Mobile and Web Combined"))
         ),
       mainPanel(
-        plotlyOutput("barplot")
+        plotlyOutput("barplot"),
+        br(),
+        h4(tags$b("Summary:")),
+        p(paste0("The three charts about 'Gender vs. Likes Received' produce ",
+                 "similar correlations. On mobile, females get an average of ",
+                 "106 more likes than males; on web, females get an average ",
+                 "of 77 more likes than males. Combining both, females get an ",
+                 "average of 183 more likes than males. From the results, we ",
+                 "can safely conclude that females generally receive more ",
+                 "likes on their Facebook profiles than males. We predict ",
+                 "this patterns occurs because females generally post ",
+                 "more contents on social media, particularly those that ",
+          "are attractive such as selfies, which brings in more attention."))
       )
     )
   ),
-  
+
   tabPanel("Age and Friendships",
     sidebarLayout(
       sidebarPanel(
-        radioButtons("friendships", "Select friendships measure type",
+        radioButtons("friendships", "Select friendship measure type",
                 c("Friendships Initiated", "Friend Count")),
         checkboxGroupInput("age_friendships", "Select age group",
-                c("10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+"), selected = "10-19")
+                c("10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+"),
+                selected = "10-19")
         ),
       mainPanel(
-        plotlyOutput("scatterplot1")
+        plotlyOutput("scatterplot1"),
+        br(),
+        h4(tags$b("Summary:")),
+        p(paste0("The two charts against the variables median friendships ",
+                 "initiated and median friend count both reveal similar ",
+                 "scatterplot patterns. In both of them, there appears to be ",
+                 "slight raise in the earlier ages, then stabilizes in the ",
+                 "middle ages, and increases greatly in the upper ages. We ",
+                 "predict this pattern occurs because teenagers and young ",
+                 "adults tend to use social media more, so it would make ",
+                 "sense why there is greater friend count and friendships ",
+                 "initiated. It may stabilize into the middle-aged years ",
+                 "possibly because people in this demographic may tend to ",
+                 "restrict their social media circles to a smaller group of ",
+                 "friends. Finally, individuals who are older may have more ",
+                 "time to use Facebook, as this social media has also gotten ",
+                 "more popular among older demographics. This may explain the ",
+                 "high count of friendships for them."))
       )
     )
   ),
-  
+
   tabPanel("Likes and Likes Received",
     sidebarLayout(
       sidebarPanel(
         checkboxGroupInput("age_likes", "Select age group",
-                           c("10-19", "20-29", "30-39", "40-49", "50-59", "60-69", "70+"), selected = "10-19")
+                        c("10-19", "20-29", "30-39", "40-49", "50-59", "60-69",
+                             "70+"), selected = "10-19")
         ),
       mainPanel(
-        plotlyOutput("scatterplot2")
+        plotlyOutput("scatterplot2"),
+        br(),
+        h4(tags$b("Summary:")),
+        p(paste0("For this graph we wanted to visualize the correlation ",
+                 "between the number of likes users give and receive. We ",
+                 "expected them to be positively correlated, meaning the more ",
+                 "likes a user gives, the more likes they will receive on ",
+                 "their own profiles. However, the plot shows a random ",
+                 "distribution, which means no correlation is found between ",
+                 "likes given and received; our previous assumption was ",
+                 "therefore refuted. We observed an interesting pattern ",
+                 "though: by selecting different age groups, we found that ",
+                 "young users tend to give and receive more likes than ",
+                 "people of older age groups; this may be due to younger ",
+                 "users being more active on social media."))
       )
     )
   )
 )
-      
-      
-      
-    
-  
-  
-  
